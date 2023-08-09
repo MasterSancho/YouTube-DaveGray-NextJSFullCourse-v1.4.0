9dee6626777f6d0e912d51bf290b677937dac204
maltsevalex <57605252+MasterSancho@users.noreply.github.com>
2023-08-09 14:28:25 +0300

    002-->Chapter3:FetchingData->1:22:00

--- next05/app/users/page.tsx
+++ next05/app/users/page.tsx
@@ -1,14 +1,36 @@
+import Link from 'next/link';
 import type { Metadata } from 'next';
-import getAllUsers from '../lib/getAllUsers';
+import getAllUsers from '@/lib/getAllUsers';
 
 export const metadata: Metadata = {
  title: 'Users',
 };
 
-export default function UsersPage() {
- return (
-  <div>
-   <h1>Users</h1>
-  </div>
+export default async function UsersPage() {
+ const usersData: Promise<User[]> = getAllUsers();
+
+ const users = await usersData;
+
+ console.log('Hello');
+
+ const content = (
+  <section>
+   <h2>
+    <Link href='/'>Back to Home</Link>
+   </h2>
+   <br />
+   {users.map((user) => {
+    return (
+     <>
+      <p key={user.id}>
+       <Link href={`/users/${user.id}`}>{user.name}</Link>
+      </p>
+      <br />
+     </>
+    );
+   })}
+  </section>
  );
+
+ return content;
 }
